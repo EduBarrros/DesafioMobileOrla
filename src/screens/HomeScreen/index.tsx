6d44ba0458387cd335e578a5d26a1596e5fe8c3e
EduBarrros <eduardodeoliveirabarros@gmail.com>
2024-01-02 16:01:12 -0300

    Feat: Base navigation, folder structure and ts config added

---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as S from './styles';
+
+const HomeScreen = () => {
+    return(
+        <S.SimpleText>
+            Home Screen
+        </S.SimpleText>
+    )
+}
+
+export default HomeScreen;
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Text } from "react-native";
+
+export const SimpleText = styled(Text)`
+
+`
