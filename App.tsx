35c524108ed53e0fc4fc3b4ea4895982dd3f507f
EduBarrros <eduardodeoliveirabarros@gmail.com>
2024-01-03 19:47:29 -0300

    Folders: routers folder added and configured

--- App.tsx
+++ App.tsx
@@ -1,28 +1,11 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Toast from 'react-native-toast-message';
-
-import { HomeScreen } from './src/screens';
+import AppNavigator from './src/routes';
 
 export default function App() {
-
-  const Stack = createNativeStackNavigator();
-
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false
-          }}
-        >
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator />
       <Toast />
     </>
   );
