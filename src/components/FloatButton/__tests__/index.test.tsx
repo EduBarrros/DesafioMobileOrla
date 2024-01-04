d419761e7aad11f02500b00421fc423ae1a7ef83
EduBarrros <eduardodeoliveirabarros@gmail.com>
2024-01-03 22:07:14 -0300

    Test: test config added

--- src/components/FloatButton/__tests__/index.test.tsx
+++ src/components/FloatButton/__tests__/index.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import FloatButton from '..';
-test("O componente renderizou", () => {
 
-    render(<FloatButton onPress={() => null}/>)
+describe("FloatButton", () => {
+    test("O componente renderizou", () => {
+        render(<FloatButton onPress={() => null} />)
+    })
 })
